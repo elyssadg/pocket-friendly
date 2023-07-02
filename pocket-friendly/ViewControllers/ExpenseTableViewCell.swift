106405838897b5ab413d7f4d9f81b3b001ff2caa
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-07-02 20:41:30 +0700

    first commit

---
+++ pocket-friendly/ViewControllers/ExpenseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpenseTableViewCell.swift
+//  pocket-friendly
+//
+//  Created by Elyssa Davina Giovanni on 25/04/23.
+//
+
+import UIKit
+
+class ExpenseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
